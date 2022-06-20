0fd3f9b58feaf950eb071ba111a850322674206a
e-roy <ericroupe@gmail.com>
2022-06-20 15:13:58 -0400

    adding vite

---
+++ packages/create-web3/templates/vite/default/src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { GetGreeter, SetGreeter } from "./components";
+
+function App() {
+  return (
+    <div className="">
+      <header style={{ padding: "1rem" }}>
+        <ConnectButton />
+      </header>
+      <main
+        style={{
+          minHeight: "60vh",
+          flex: "1",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <GetGreeter />
+        <SetGreeter />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/create-web3/templates/vite/default/src/config.js
@@ -0,0 +1,2 @@
+export const NETWORK_ID = 31337;
+export const NETWORK_NAME = 'localhost';
